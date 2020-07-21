554b6b9ea58ee848fed869be73baf78d0261bd88
Ferm0494 <ferm0494@gmail.com>
2020-07-21 15:40:17 -0600

    Add draw logic into game

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,33 +31,68 @@ const gameBoard = () => {
 
 const player = (name, val) => {
     // Here is is to check the game logic
-    const checkWin = ({board}) => {
-      if (board[0] !== '' && board[0] === board[1] && board[1] === board[2] ){
-        return true
-      }
-      if (board[3] !== '' && board[3] === board[4] && board[4] === board[5] ){
-        return true
-      }
-      if (board[6] !== '' && board[6] === board[7] && board[7] === board[8] ){
-        return true
-      }
-      if (board[0] !== '' && board[0] === board[3] && board[3] === board[6] ){
-        return true
-      }
-      if (board[1] !== '' && board[1] === board[4] && board[4] === board[7] ){
-        return true
-      }
-      if (board[2] !== '' && board[2] === board[5] && board[5] === board[8] ){
-        return true
-      }
-      if (board[0] !== '' && board[0] === board[4] && board[4] === board[8] ){
-        return true
-      }
-      if (board[2] !== '' && board[2] === board[4] && board[4] === board[6] ){
-        return true
-      }
-    return false
+    const checkWin = (
+        gameBoard, header
+    ) => {
+        let board = gameBoard.board
+        if (board[0] !== '' && board[0] === board[1] && board[1] === board[2]) {
+            renderWinner(gameBoard, header)
+        }
+        if (board[3] !== '' && board[3] === board[4] && board[4] === board[5]) {
+            renderWinner(gameBoard, header)
+        }
+        if (board[6] !== '' && board[6] === board[7] && board[7] === board[8]) {
+            renderWinner(gameBoard, header)
+        }
+        if (board[0] !== '' && board[0] === board[3] && board[3] === board[6]) {
+            renderWinner(gameBoard, header)
+        }
+        if (board[1] !== '' && board[1] === board[4] && board[4] === board[7]) {
+            renderWinner(gameBoard, header)
+        }
+        if (board[2] !== '' && board[2] === board[5] && board[5] === board[8]) {
+            renderWinner(gameBoard, header)
+        }
+        if (board[0] !== '' && board[0] === board[4] && board[4] === board[8]) {
+            renderWinner(gameBoard, header)
+        }
+        if (board[2] !== '' && board[2] === board[4] && board[4] === board[6]) {
+            renderWinner(gameBoard, header)
+        }
 
+
+        return false
+
+    }
+
+    const renderWinner = ({
+        slots,
+        board
+    }, header) => {
+        header.classList.add('d-none')
+        for (let i = 0; i < slots.length; i += 1) {
+            slots[i].classList.add('d-none')
+        }
+        let pop = document.getElementsByClassName('rounded')[0]
+        pop.classList.add('d-block')
+
+        document.body.style.background = '#f6f6f6'
+        let children = pop.children
+        let popUp = children[0]
+
+        let drawArray = board.filter(x => {
+            return x !== ''
+        })
+
+        if (drawArray.length === 9) {
+            console.log("Game drawed!")
+        }
+        console.log(`this is drawArray ${drawArray}`)
+
+        popUp.innerHTML = `${name} WINS!`
+        children[1].addEventListener('click', function() {
+            location.reload()
+        })
     }
     return {
         checkWin,
@@ -69,22 +104,23 @@ const player = (name, val) => {
 const response = (index, player, board, slots, header, status) => {
     let response = board.modifyArray(index, player.val)
     if (response) {
-
-        header.innerHTML = `${player.name} turn`
         slots[index].innerHTML = player.val
-        if (player.checkWin(board)){
-          alert(`${player.name} is the winner!`)
-        }
-        return !status
+        status = !status
     } else {
         alert("Please choose another position")
-        return status
 
+
+    }
+
+    return {
+        status,
+        player
     }
 
 }
 
 const init = () => {
+
     let header = document.getElementsByTagName('h5')[0]
     let board = gameBoard()
     let player1 = player('Player 1', 'X')
@@ -95,10 +131,33 @@ const init = () => {
     for (let i = 0; i < board.slots.length; i++) {
         board.slots[i].addEventListener('click', function() {
             if (player1turn) {
-                player1turn = response(i, player1, board, board.slots, header, player1turn)
+
+                let {
+                    status,
+                    player
+                } = response(i, player1, board, board.slots, header, player1turn)
+                player1turn = status
+                if (!player1turn) {
+                    header.innerHTML = `${player2.name} Turn`
+                }
+
+                player.checkWin(board, header)
+
+                // alert(`${player.name} is the winner!`)
 
             } else {
-                player1turn = response(i, player2, board, board.slots, header, player1turn)
+
+                let {
+                    status,
+                    player
+                } = response(i, player2, board, board.slots, header, player1turn)
+                player1turn = status;
+                if (player1turn) {
+                    header.innerHTML = `${player1.name} Turn`
+                }
+
+                player.checkWin(board, header)
+
             }
         })
     }
