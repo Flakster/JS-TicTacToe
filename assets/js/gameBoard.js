272ad02aaf94e9acd1f725e9b03686cc63f68289
Ferm0494 <ferm0494@gmail.com>
2020-07-22 12:11:48 -0600

    Remove unnec function displayArry from gameBoard

--- assets/js/gameBoard.js
+++ assets/js/gameBoard.js
@@ -1,13 +1,7 @@
 const gameBoard = () => {
   const board = ['', '', '', '', '', '', '', '', ''];
   const slots = document.getElementsByClassName('slot');
-  // Verify use of this function ---> OPTIONAL.
-  const displayArray = () => {
-    for (let i = 0; i < board.length; i += 1) {
-      slots[i].innerHTML = board[i];
-    }
-  };
-  // Work on this function
+
   const modifyArray = (i, val) => {
     if (board[i].length === 0 && val.length !== 0) {
       board[i] = val;
@@ -19,7 +13,6 @@ const gameBoard = () => {
 
   return {
     slots,
-    displayArray,
     modifyArray,
     board,
   };
