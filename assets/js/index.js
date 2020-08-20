2ee89c747f0f9b9dd6a7baab38f01edd1e26372b
Ferm0494 <ferm0494@gmail.com>
2020-08-20 14:25:24 -0600

    Webpack Config on tictactoe

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,75 +1,77 @@
-
 /* eslint-disable import/extensions */
-
-import player from './player.js';
-import gameBoard from './gameBoard.js';
-import DOM from './objectsDOM.js';
-
+import player from "./player.js";
+import gameBoard from "./gameBoard.js";
+import DOM from "./objectsDOM.js";
+import "../stylesheets/style.css";
 /* eslint-enable import/extensions */
 
 const response = (index, player, board, slot, status) => {
-  const response = board.modifyArray(index, player.val);
-  if (response) {
-    slot.innerHTML = player.val;
-    status = !status;
-    slot.style.cursor = 'default';
-  }
+    const response = board.modifyArray(index, player.val);
+    if (response) {
+        slot.innerHTML = player.val;
+        status = !status;
+        slot.style.cursor = "default";
+    }
 
-  return {
-    status,
-    player,
-  };
+    return {
+        status,
+        player
+    };
 };
-
 
 const init = (name1, name2) => {
-  DOM().modifyElementsDOM(name1);
-  const board = gameBoard();
-  const player1 = player(name1, 'X');
-  const player2 = player(name2, 'O');
-  let player1turn = true;
-  Array.from(DOM().slots).forEach((slot, i) => {
-    slot.addEventListener('click', () => {
-      if (player1turn) {
-        const {
-          status,
-          player,
-        } = response(i, player1, board, slot, player1turn);
-        player1turn = status;
-        if (!player1turn) {
-          DOM().header.innerHTML = `${player2.name} Turn`;
-        }
+    DOM().modifyElementsDOM(name1);
+    const board = gameBoard();
+    const player1 = player(name1, "X");
+    const player2 = player(name2, "O");
+    let player1turn = true;
+    Array.from(DOM().slots).forEach((slot, i) => {
+        slot.addEventListener("click", () => {
+            if (player1turn) {
+                const { status, player } = response(
+                    i,
+                    player1,
+                    board,
+                    slot,
+                    player1turn
+                );
+                player1turn = status;
+                if (!player1turn) {
+                    DOM().header.innerHTML = `${player2.name} Turn`;
+                }
 
-        board.checkWin(player);
-      } else {
-        const {
-          status,
-          player,
-        } = response(i, player2, board, slot, player1turn);
-        player1turn = status;
-        if (player1turn) {
-          DOM().header.innerHTML = `${player1.name} Turn`;
-        }
+                board.checkWin(player);
+            } else {
+                const { status, player } = response(
+                    i,
+                    player2,
+                    board,
+                    slot,
+                    player1turn
+                );
+                player1turn = status;
+                if (player1turn) {
+                    DOM().header.innerHTML = `${player1.name} Turn`;
+                }
 
-        board.checkWin(player);
-      }
+                board.checkWin(player);
+            }
+        });
     });
-  });
 };
 
+const startBtn = document.getElementById("startBtn");
+startBtn.addEventListener("click", e => {
+    e.preventDefault();
+    const inputs = document.getElementsByTagName("input");
 
-const startBtn = document.getElementById('startBtn');
-startBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  const inputs = document.getElementsByTagName('input');
-
-  if (inputs[0].value.length !== 0 && inputs[1].value.length !== 0) {
-    init(inputs[0].value, inputs[1].value);
-  } else if (inputs[0].value === '') {
-    inputs[0].setCustomValidity('Cant be empty');
-    inputs[0].reportValidity();
-  } else {
-    inputs[1].setCustomValidity('Cant be empty');
-    inputs[1].reportValidity();
-  }
+    if (inputs[0].value.length !== 0 && inputs[1].value.length !== 0) {
+        init(inputs[0].value, inputs[1].value);
+    } else if (inputs[0].value === "") {
+        inputs[0].setCustomValidity("Cant be empty");
+        inputs[0].reportValidity();
+    } else {
+        inputs[1].setCustomValidity("Cant be empty");
+        inputs[1].reportValidity();
+    }
 });
